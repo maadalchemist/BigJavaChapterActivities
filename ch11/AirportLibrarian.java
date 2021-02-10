20f7a79726ff6e4964ba9c0c100ae260b3239fbc
maad_alchemist <josephwardle02@gmail.com>
2021-02-10 12:02:55 -0700

    stuff

--- ch11/AirportLibrarian.java
+++ ch11/AirportLibrarian.java
@@ -6,7 +6,22 @@ import java.lang.Math;
 
 public class AirportLibrarian {
     public static void main(String[] args) throws FileNotFoundException {
+        Scanner cin = new Scanner(System.in);
         Scanner airportsDat = new Scanner(new File("airports.dat"));
+        airportsDat.useDelimiter("");
+        String in = "";
+        String city = "";
+        String name = "";
+        char c = '';
+
+        System.out.println("Enter a city:");
+        in = cin.next();
+
+        int cycle = 1;
+        while(airportsDat.hasNext()) {
+            c = airportsDat.next();
+            
+        }
     }
 }
 
