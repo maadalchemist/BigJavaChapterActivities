ef25f0e90e1d638cfd40dcf2f1f5b70f7a225fb2
maad_alchemist <josephwardle02@gmail.com>
2021-02-10 11:04:55 -0700

    stuff

---
+++ ch11/Main.java
@@ -0,0 +1,13 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.Scanner;
+import java.lang.Math;
+
+public class AirportLibrarian {
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner airportsDat = new Scanner(new File("airports.dat"));
+    }
+}
+
+// 7868     "Camp Bastion Airport"     "Camp Bastion"     "Afghanistan"     "OAZ"     "OAZI"     31.863800048799998     64.2246017456     2943     4.5     "N"     "Asia/Kabul"     "airport"     "OurAirports"
