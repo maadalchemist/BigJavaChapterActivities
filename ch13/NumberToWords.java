83c66c8cb30b01baf5da4aed015f75745e575c4e
maad_alchemist <josephwardle02@gmail.com>
2021-03-11 12:03:50 -0700

    stuff

---
+++ ch13/NumberToWords.java
@@ -0,0 +1,19 @@
+public class NumberToWords {
+    private String num;
+    private ArrayList<String> wordList;
+
+    public NumberToWords(String num) {
+        this.num = num;
+        this.wordlist = ;
+    }
+
+
+}
+
+// Phone numbers and PIN codes can be easier to remember when you find words that spell out the number on a standard phone pad.
+// For example, instead of remembering the combination 5282, you can just think of JAVA.
+//
+// Write a recursive method that, given a number, yields all possible spellings (which may or may not be real words).
+//
+// checking the words against the /usr/share/dict/words file on your computer, or the words.txt file in the companion code for this book.
+// For a given number, return only actual words.
